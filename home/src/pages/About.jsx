27beef1941d040ef48234cdb2df1cb729c4880b9
Alim Ansari <kkumar58647@gmail.com>
2026-07-30 22:19:35 +0530

    content change

--- home/src/pages/About.jsx
+++ home/src/pages/About.jsx
@@ -1,18 +1,74 @@
-import React from 'react'
-import './About.css'
+import React from "react";
+import { FaEnvelope, FaPhoneAlt, FaMapMarkerAlt } from "react-icons/fa";
+import "./About.css";
 
 function About() {
   return (
-   <div className='contacta'>
-      <h1>Contact Us</h1>
-      <form action="https://formspree.io/f/mwpypgqe" method='POST'>
-        <input type="text" placeholder='Enter Your Name...' name='username' required />
-        <input type="Email" placeholder='email' name='email' required />
-        <textarea name="" placeholder='message' required></textarea>
-        <button>Submit</button>
-      </form>
-    </div>
-  )
+    <section className="contact-section">
+      <div className="contact-container">
+
+        <div className="contact-info">
+          <h1>Contact Us</h1>
+          <p>
+            Have any questions about buying, selling, or renting a property?
+            We'd love to hear from you. Fill out the form and we'll get back to
+            you as soon as possible.
+          </p>
+
+          <div className="info">
+            <p>
+              <FaEnvelope /> alimansari.dev@gmail.com
+            </p>
+
+            <p>
+              <FaPhoneAlt /> +91 70xxx 43210
+            </p>
+
+            <p>
+              <FaMapMarkerAlt /> New Delhi, India
+            </p>
+          </div>
+        </div>
+
+        <form
+          className="contact-form"
+          action="https://formspree.io/f/mwpypgqe"
+          method="POST"
+        >
+          <input
+            type="text"
+            name="username"
+            placeholder="Full Name"
+            required
+          />
+
+          <input
+            type="email"
+            name="email"
+            placeholder="Email Address"
+            required
+          />
+
+          <input
+            type="text"
+            name="subject"
+            placeholder="Subject"
+            required
+          />
+
+          <textarea
+            name="message"
+            rows="6"
+            placeholder="Write your message..."
+            required
+          ></textarea>
+
+          <button type="submit">Send Message</button>
+        </form>
+
+      </div>
+    </section>
+  );
 }
 
-export default About
+export default About;
