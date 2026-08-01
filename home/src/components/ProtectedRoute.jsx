e50706f9d0021fbf6d9532170fb5fa88527a1202
Alim Ansari <kkumar58647@gmail.com>
2026-08-01 12:17:45 +0530

    Add uploads folder

--- home/src/components/ProtectedRoute.jsx
+++ home/src/components/ProtectedRoute.jsx
@@ -1,27 +1,27 @@
-// import { Navigate } from "react-router-dom";
+import { Navigate } from "react-router-dom";
 
-// function ProtectedRoute({ children }) {
+function ProtectedRoute({ children }) {
 
-//   const token = localStorage.getItem("accessToken");
-
-//   if (!token) {
-//     return <Navigate to="/login" />;
-//   }
-
-//   return children;
-// }
-
-// export default ProtectedRoute;
-
-function ProtectedRoute({ children, onLoginRequired }) {
   const token = localStorage.getItem("accessToken");
 
   if (!token) {
-    onLoginRequired(); // Login Popup खोलो
-    return null;
+    return <Navigate to="/login" />;
   }
 
   return children;
 }
 
 export default ProtectedRoute;
+
+// function ProtectedRoute({ children, onLoginRequired }) {
+//   const token = localStorage.getItem("accessToken");
+
+//   if (!token) {
+//     onLoginRequired(); // Login Popup खोलो
+//     return null;
+//   }
+
+//   return children;
+// }
+
+// export default ProtectedRoute;
